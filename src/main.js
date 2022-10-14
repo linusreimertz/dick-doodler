db7b99d46e10db3213f98271e9ee5a16f660dbee
Linus Reimertz <linus.reimertz2@gmail.com>
2022-10-14 16:10:26 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import "./app.css";
+import App from "./App.svelte";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) =>
+      console.log(
+        "service worker registered",
+        reg
+      )
+    )
+    .catch((err) =>
+      console.log(
+        "service worker not registered",
+        err
+      )
+    );
+}
+
+const app = new App({
+  target: document.getElementById("app")
+});
+
+export default app;
